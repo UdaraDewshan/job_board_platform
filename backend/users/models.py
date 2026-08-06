6445c15ed7de0c70a61316de8e4a5267c28c572a
UdaraDewshan <dewudara@gmail.com>
2026-08-06 14:46:43 +0530

    Add CustomUser model and configure PostgreSQL connection via .env

---
+++ backend/users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('employer', 'Employer'),
+        ('job_seeker', 'Job_Seeker'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='job_seeker')
+
+    def __str__(self):
+        return f"{self.usermame} ({self.role})"
+    
