91190a6de0c4f92e6e31f9685734619fccf58c45
UdaraDewshan <dewudara@gmail.com>
2026-08-08 22:23:20 +0530

    Implement authentication API with JWT support and user registration serializer

---
+++ backend/users/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class UserSerializer(serializers.ModelSerializer):   
+    class Meta:
+        model = CustomUser
+        fields = ('id','username','email','role')
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=true)  #paassword not expose
+
+    class Meta:
+        model = CustomUser
+        filels = ('usernaem','email','password','role')
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email',''),
+            password=validated_data['password'],
+            role=validated_data.get('role', 'job_seeker')
+        )
+        return user
