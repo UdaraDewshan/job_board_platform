90f7e26c339b7706fa90cbcf01388ae28b1524ba
UdaraDewshan <dewudara@gmail.com>
2026-08-10 19:57:13 +0530

    Add User Profile API (Model, Serializer, and View)

--- backend/users/models.py
+++ backend/users/models.py
@@ -12,3 +12,13 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return f"{self.username} ({self.role})"
 
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
+
+    bio = models.TextField(blank=True, null=True)
+    skills = models.CharField(max_length=255, blank=True, null=True)
+    resume_link = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
