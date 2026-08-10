8c08a9920d6409a2d24ff6a52cf81157ba83666f
UdaraDewshan <dewudara@gmail.com>
2026-08-10 23:23:49 +0530

    Add JobSerializer and JobListCreateView

--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Job
+from .serializers import JobSerializer
 
-# Create your views here.
+class JobListCreateView(generics.ListCreateAPIView):
+
+    queryset = Job.objects.all().order_by('-created_at')
+    serializer_class = JobSerializer
+    
+    permission_classes = [IsAuthenticatedOrReadOnly] 
+
+    def perform_create(self, serializer):
+        serializer.save(employer=self.request.user)
